56d5e7ace722d3ae39a773afbee83f9725f1a6dd
abntaha061 <abntaha061@gmail.com>
2026-06-01 08:57:57 +0300

    Update MainActivity.kt

--- com/mohamed/pdfreader/MainActivity.kt
+++ com/mohamed/pdfreader/MainActivity.kt
@@ -6,17 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import com.mohamed.pdfreader.ui.screens.MainScreen
 import com.mohamed.pdfreader.ui.theme.PdfReaderTheme
+import com.mohamed.pdfreader.data.local.SettingsManager
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var settingsManager: SettingsManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PdfReaderTheme {
+            // الاستماع لحالة الثيم من الإعدادات
+            val isDarkMode by settingsManager.isDarkMode.collectAsState()
+            
+            PdfReaderTheme(darkTheme = isDarkMode) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
