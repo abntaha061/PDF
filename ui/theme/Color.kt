eb67d424235c4534172993d335d544d19ac223cd
abntaha061 <abntaha061@gmail.com>
2026-06-01 08:19:01 +0300

    Create Color.kt

---
+++ ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.mohamed.pdfreader.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF1565C0)
+val BackgroundDark = Color(0xFF1E1E1E)
+val SurfaceDark = Color(0xFF2D2D2D)
+
+val AccentGold = Color(0xFFF59E0B)
+val SuccessGreen = Color(0xFF22C55E)
+val ErrorRed = Color(0xFFEF4444)
+
+val TextPrimaryDark = Color(0xFFE2E8F0)
+val TextMutedDark = Color(0xFF64748B)
+
+val BackgroundLight = Color(0xFFF8FAFC)
+val SurfaceLight = Color(0xFFFFFFFF)
+val TextPrimaryLight = Color(0xFF0F172A)
