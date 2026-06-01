b6ddb43493f487dc43e20b6962b2c5c44c03e188
abntaha061 <abntaha061@gmail.com>
2026-06-01 08:22:15 +0300

    Create MainActivity.kt

---
+++ com/mohamed/pdfreader/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mohamed.pdfreader
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import com.mohamed.pdfreader.ui.screens.MainScreen
+import com.mohamed.pdfreader.ui.theme.PdfReaderTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PdfReaderTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
